01cc5c797ac39cc2e3bb3d28a83f84ade2cf6aad
balachander angalla <bangalla0916@gmail.com>
2022-06-01 21:54:31 -0500

    updated lamda handler

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,13 +1,16 @@
 import json
-import sys
-import boto3
 import urllib.parse
-from revenue import SearchEngineRevenue
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    #logfile = 'data.tsv'
-     # Get the object from the event and show its content type
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
