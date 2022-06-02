2b7ec7e13a4b89ea94a72ee7e94354fb41ae09f2
balachander angalla <bangalla0916@gmail.com>
2022-06-01 22:48:45 -0500

    updated lamda handler

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,7 @@
 import json
 import urllib.parse
 import boto3
+import pandas as pd
 
 print('Loading function')
 
@@ -17,8 +18,9 @@ def lambda_handler(event, context):
     print(key)
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        df = pd.read_csv(io.BytesIO(response['Body'].read()))
+        print(df)
+        return df
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
