1b99cb83aca5d7d2ee26f5ae30b36463b195e240
balachander angalla <bangalla0916@gmail.com>
2022-06-04 16:39:53 -0500

    added unit test cases and logging

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,9 +3,11 @@ import urllib.parse
 import boto3
 import pandas as pd
 import io
+import os, sys
 import logging
 from mock import patch, Mock
 from src.revenue import SearchEngineRevenue
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..","..",'src'))
 logging.basicConfig(filename ='app.log', level = logging.ERROR)
 
 
